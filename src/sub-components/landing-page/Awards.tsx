20d215b320456fff49d0dbbe4eafd523b02ac494
sk <skkayes13@gmail.com>
2025-10-16 13:11:54 +0530

    add Awards.tsx

--- src/sub-components/landing-page/Awards.tsx
+++ src/sub-components/landing-page/Awards.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
 
 const Awards = () => {
-  return (
-    <div>
-      award
-    </div>
-  )
+    return (
+        <div className='container mt-3'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src="svg/largestBroker.svg" alt="" />
+                </div>
+                <div className='col-6 mt-5'>
+                    <h1>Largest stock broker in India</h1>
+                    <p className='mt-4'>2+ million Zerodha clients contribute to over 15% of all retauk order volumes in india daily by trading and inversting in:</p>
+                    <div className='row mt-3'>
+                        <div className='col-6'>
+                            <ul>
+                                <li>
+                                    <p>Fetures and Options</p>
+                                </li>
+                                <li>
+                                    <p>Commodity derivatives</p>
+                                </li>
+                                <li>
+                                    <p>Currency derivatives</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='col-6'>
+                            <ul>
+                                <li>
+                                    <p>Stocks & IPOs</p>
+                                </li>
+                                <li>
+                                    <p>Direct mutual funds</p>
+                                </li>
+                                <li>
+                                    <p>Bonds and Govt. Securities</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <img src='images/pressLogos.png' style={{width: "80%", marginTop: "20px"}} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Awards
--- src/sub-components/landing-page/Hero.tsx
+++ src/sub-components/landing-page/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
         <img src='images/homeHero.png' alt='Hero Image' />
         <h1 className='mt-5'>Invest in everything</h1>
         <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-        <button className='p-2 btn btn-primary fs-5' style={{width: "20%", margin: "0 auto"}}>Sign up for free</button>
+        <button className='p-2 btn btn-primary fs-5 mb-5' style={{width: "20%", margin: "0 auto"}}>Sign up for free</button>
       </div>
     </div>
   )
